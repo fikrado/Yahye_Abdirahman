21b418926bbac35138c3a18c88af05d485fff4f9
Yahye Abdirahman <68783841+fikrado@users.noreply.github.com>
2021-03-16 13:50:29 +0300

    best

---
+++ yahye.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+#colours
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+b='\033[1;34m'
+p='\033[1;35m'
+reset='\033[0m'
+echo " "
+echo " $red
+
+ _           _        _ _ _             
+(_)_ __  ___| |_ __ _| | (_)_ __   __ _ 
+| | '_ \/ __| __/ _` | | | | '_ \ / _` |
+| | | | \__ | || (_| | | | | | | | (_| |
+|_|_| |_|___/\__\__,_|_|_|_|_| |_|\__, |
+                                  |___/ 
+"
+
+echo "$yellow                +++++++++++++~ "
+echo " "
+echo "$green              <--[Coded by Yahye Abdirahman]-->"
+
+echo "$cyan                ********** "
+echo "$green              <~(TIKTOK- Fikrado)~>"
+echo " "
+echo "$yellow                +++++++++++++~ "
+sleep 3.50
+echo " "
+echo  $p 'Please Wait ===+['$b'./                   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'..\                  '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.../                 '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'....\                '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'...../               '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'......./             '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........\            '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........../          '$p']\'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'..........\          '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']|'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'..............\      '$p']/'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']\'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'...............\     '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'................/    '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................\   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................../ '$p']|'
+sleep 0.4
+clear
+
+echo  "$yellow+++++++++++>$p[Please Wait]$yellow<+++++++++++"
+sleep 0.4
+echo " "
+cd $HOME
+
+apt install wget bash python python2 curl mpv figlet -y
+pip install lolcat 
+pip2 install requests mechanize
+
+figlet HACKING_TOOLS | lolcat 
+git clone https://github.com/fikrado/fikrado.py && git clone https://github.com/fikrado/qurxin && git clone https://github.com/fikrado/ip 
+git clone https://github.com/fikrado/JOKER-burtal-force && git clone https://github.com/fikrado/fikrado.txt
+
+clear
+
+figlet Kali_tools | lolcat 
+pkg install x11-repo wireshark-gtk xterm  tigervnc tigervnc-viewer nmap zip -y
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip && unzip ngrok-stable-linux-arm.zip
+
+figlet Done | lolcat
+
+cd qurxin && chmod +x * && sh install.sh
+
+
+
